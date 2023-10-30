2606d428b485a9d833369c6b1e6dbdb747c9a094
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-30 23:10:59 +0300

    imported Header and Login component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Client Auth Projesi: Friends</h1>
+    <div className="bg-slate-200 App">
+      <Header/>
+      <Login/>
     </div>
   );
 }
